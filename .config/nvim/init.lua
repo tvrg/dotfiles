ba45e54d335261046d7259d60d3a40851bc56344
Thomas von Rosenberg <tvr@fact-finder.com>
2021-09-29 10:54:01 +0200

    nvim: fix nvim-tree config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -60,7 +60,6 @@ require('packer').startup(function()
     use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'} -- syntax tree parser
     use 'windwp/nvim-ts-autotag' -- close html tags via treesitter
     use 'JoosepAlviste/nvim-ts-context-commentstring'
-    use 'nvim-treesitter/nvim-treesitter'
     use 'nvim-treesitter/nvim-treesitter-textobjects'
     -- cool but really slow
     -- use 'haringsrob/nvim_context_vt' -- show context on closing brackets
@@ -264,60 +263,7 @@ require'compe'.setup {
 }
 
 -- file drawer
-
-vim.g.nvim_tree_side = "left"
-vim.g.nvim_tree_width = 30
-vim.g.nvim_tree_ignore = {".git", "node_modules", ".cache"}
-vim.g.nvim_tree_gitignore = 1
-vim.g.nvim_tree_auto_ignore_ft = {"dashboard"} -- don't open tree on specific fiypes.
-vim.g.nvim_tree_auto_open = 0
-vim.g.nvim_tree_auto_close = 0
-vim.g.nvim_tree_quit_on_open = 0 -- closes tree when file's opened
-vim.g.nvim_tree_follow = 1
-vim.g.nvim_tree_indent_markers = 1
-vim.g.nvim_tree_hide_dotfiles = 1
-vim.g.nvim_tree_git_hl = 0
-vim.g.nvim_tree_highlight_opened_files = 0
-vim.g.nvim_tree_root_folder_modifier = table.concat {
-    ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??"
-}
-vim.g.nvim_tree_tab_open = 0
-vim.g.nvim_tree_allow_resize = 1
-vim.g.nvim_tree_add_trailing = 0 -- append a trailing slash to folder names
-vim.g.nvim_tree_disable_netrw = 1
-vim.g.nvim_tree_hijack_netrw = 0
-vim.g.nvim_tree_update_cwd = 0
 vim.g.nvim_tree_show_icons = {git = 0, folders = 1, files = 0}
-
-local tree_cb = require'nvim-tree.config'.nvim_tree_callback
-vim.g.nvim_tree_bindings = {
-    {key = {"<CR>", "o", "<2-LeftMouse>"}, cb = tree_cb "edit"},
-    {key = {"<2-RightMouse>", "<C-]>"}, cb = tree_cb "cd"},
-    {key = "<C-v>", cb = tree_cb "vsplit"},
-    {key = "<C-x>", cb = tree_cb "split"},
-    {key = "<C-t>", cb = tree_cb "tabnew"},
-    {key = "<", cb = tree_cb "prev_sibling"},
-    {key = ">", cb = tree_cb "next_sibling"},
-    {key = "P", cb = tree_cb "parent_node"},
-    {key = "<BS>", cb = tree_cb "close_node"},
-    {key = "<S-CR>", cb = tree_cb "close_node"},
-    {key = "<Tab>", cb = tree_cb "preview"},
-    {key = "K", cb = tree_cb "first_sibling"},
-    {key = "J", cb = tree_cb "last_sibling"},
-    {key = "I", cb = tree_cb "toggle_ignored"},
-    {key = "H", cb = tree_cb "toggle_dotfiles"},
-    {key = "R", cb = tree_cb "refresh"}, {key = "a", cb = tree_cb "create"},
-    {key = "d", cb = tree_cb "remove"}, {key = "r", cb = tree_cb "rename"},
-    {key = "<C-r>", cb = tree_cb "full_rename"},
-    {key = "x", cb = tree_cb "cut"}, {key = "c", cb = tree_cb "copy"},
-    {key = "p", cb = tree_cb "paste"}, {key = "y", cb = tree_cb "copy_name"},
-    {key = "Y", cb = tree_cb "copy_path"},
-    {key = "gy", cb = tree_cb "copy_absolute_path"},
-    {key = "[c", cb = tree_cb "prev_git_item"},
-    {key = "}c", cb = tree_cb "next_git_item"},
-    {key = "-", cb = tree_cb "dir_up"}, {key = "q", cb = tree_cb "close"},
-    {key = "g?", cb = tree_cb "toggle_help"}
-}
 vim.g.nvim_tree_icons = {
     folder = {
         default = "",
@@ -326,6 +272,60 @@ vim.g.nvim_tree_icons = {
         empty_open = "",
         symlink = "",
         symlink_open = ""
+    }
+}
+local tree_cb = require'nvim-tree.config'.nvim_tree_callback
+require'nvim-tree'.setup {
+    nvim_tree_side = "left",
+    nvim_tree_width = 30,
+    nvim_tree_ignore = {".git", "node_modules", ".cache"},
+    nvim_tree_gitignore = 1,
+    nvim_tree_auto_ignore_ft = {"dashboard"}, -- don't open tree on specific fiypes.
+    nvim_tree_auto_open = 0,
+    nvim_tree_auto_close = 0,
+    nvim_tree_quit_on_open = 0, -- closes tree when file's opened
+    nvim_tree_follow = 1,
+    nvim_tree_indent_markers = 1,
+    nvim_tree_hide_dotfiles = 1,
+    nvim_tree_git_hl = 0,
+    nvim_tree_highlight_opened_files = 0,
+    nvim_tree_root_folder_modifier = table.concat {
+        ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??"
+    },
+    nvim_tree_tab_open = 0,
+    nvim_tree_allow_resize = 1,
+    nvim_tree_add_trailing = 0, -- append a trailing slash to folder names
+    nvim_tree_disable_netrw = 1,
+    nvim_tree_hijack_netrw = 0,
+    nvim_tree_update_cwd = 0,
+    nvim_tree_bindings = {
+        {key = {"<CR>", "o", "<2-LeftMouse>"}, cb = tree_cb "edit"},
+        {key = {"<2-RightMouse>", "<C-]>"}, cb = tree_cb "cd"},
+        {key = "<C-v>", cb = tree_cb "vsplit"},
+        {key = "<C-x>", cb = tree_cb "split"},
+        {key = "<C-t>", cb = tree_cb "tabnew"},
+        {key = "<", cb = tree_cb "prev_sibling"},
+        {key = ">", cb = tree_cb "next_sibling"},
+        {key = "P", cb = tree_cb "parent_node"},
+        {key = "<BS>", cb = tree_cb "close_node"},
+        {key = "<S-CR>", cb = tree_cb "close_node"},
+        {key = "<Tab>", cb = tree_cb "preview"},
+        {key = "K", cb = tree_cb "first_sibling"},
+        {key = "J", cb = tree_cb "last_sibling"},
+        {key = "I", cb = tree_cb "toggle_ignored"},
+        {key = "H", cb = tree_cb "toggle_dotfiles"},
+        {key = "R", cb = tree_cb "refresh"}, {key = "a", cb = tree_cb "create"},
+        {key = "d", cb = tree_cb "remove"}, {key = "r", cb = tree_cb "rename"},
+        {key = "<C-r>", cb = tree_cb "full_rename"},
+        {key = "x", cb = tree_cb "cut"}, {key = "c", cb = tree_cb "copy"},
+        {key = "p", cb = tree_cb "paste"},
+        {key = "y", cb = tree_cb "copy_name"},
+        {key = "Y", cb = tree_cb "copy_path"},
+        {key = "gy", cb = tree_cb "copy_absolute_path"},
+        {key = "[c", cb = tree_cb "prev_git_item"},
+        {key = "}c", cb = tree_cb "next_git_item"},
+        {key = "-", cb = tree_cb "dir_up"}, {key = "q", cb = tree_cb "close"},
+        {key = "g?", cb = tree_cb "toggle_help"}
     }
 }
 
@@ -454,28 +454,28 @@ require'nvim-treesitter.configs'.setup {
 }
 
 require'nvim-treesitter.configs'.setup {
-  textobjects = {
-    move = {
-      enable = true,
-      set_jumps = true, -- whether to set jumps in the jumplist
-      goto_next_start = {
-        ["]m"] = "@function.outer",
-        ["]]"] = "@class.outer",
-      },
-      goto_next_end = {
-        ["]M"] = "@function.outer",
-        ["]["] = "@class.outer",
-      },
-      goto_previous_start = {
-        ["[m"] = "@function.outer",
-        ["[["] = "@class.outer",
-      },
-      goto_previous_end = {
-        ["[M"] = "@function.outer",
-        ["[]"] = "@class.outer",
-      },
-    },
-  },
+    textobjects = {
+        move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+                ["]m"] = "@function.outer",
+                ["]]"] = "@class.outer"
+            },
+            goto_next_end = {
+                ["]M"] = "@function.outer",
+                ["]["] = "@class.outer"
+            },
+            goto_previous_start = {
+                ["[m"] = "@function.outer",
+                ["[["] = "@class.outer"
+            },
+            goto_previous_end = {
+                ["[M"] = "@function.outer",
+                ["[]"] = "@class.outer"
+            }
+        }
+    }
 }
 
 local eslint = require 'diagnosticls-configs.linters.eslint'

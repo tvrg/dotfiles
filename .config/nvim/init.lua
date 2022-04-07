68266d257dcae353c7afe6d397e004a8b84f9777
Thomas von Rosenberg <tvr@fact-finder.com>
2022-04-07 10:47:54 +0200

    nvim: update nvim-tree setup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -281,56 +281,49 @@ vim.g.nvim_tree_icons = {
 }
 local tree_cb = require'nvim-tree.config'.nvim_tree_callback
 require'nvim-tree'.setup {
-    side = "left",
-    width = 30,
-    ignore = {".git", "node_modules", ".cache"},
-    gitignore = 1,
-    auto_ignore_ft = {"dashboard"}, -- don't open tree on specific fiypes.
-    auto_open = 0,
-    quit_on_open = 0, -- closes tree when file's opened
-    follow = 1,
-    indent_markers = 1,
-    hide_dotfiles = 1,
-    git_hl = 0,
-    highlight_opened_files = 0,
-    root_folder_modifier = table.concat {
-        ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??"
+    filters = {custom = {".git", "node_modules", ".cache"}},
+    view = {
+        side = "left",
+        width = 30,
+        mappings = {
+            list = {
+                {key = {"<CR>", "o", "<2-LeftMouse>"}, cb = tree_cb "edit"},
+                {key = {"<2-RightMouse>", "<C-]>"}, cb = tree_cb "cd"},
+                {key = "<C-v>", cb = tree_cb "vsplit"},
+                {key = "<C-x>", cb = tree_cb "split"},
+                {key = "<C-t>", cb = tree_cb "tabnew"},
+                {key = "<", cb = tree_cb "prev_sibling"},
+                {key = ">", cb = tree_cb "next_sibling"},
+                {key = "P", cb = tree_cb "parent_node"},
+                {key = "<BS>", cb = tree_cb "close_node"},
+                {key = "<S-CR>", cb = tree_cb "close_node"},
+                {key = "<Tab>", cb = tree_cb "preview"},
+                {key = "K", cb = tree_cb "first_sibling"},
+                {key = "J", cb = tree_cb "last_sibling"},
+                {key = "I", cb = tree_cb "toggle_ignored"},
+                {key = "H", cb = tree_cb "toggle_dotfiles"},
+                {key = "R", cb = tree_cb "refresh"},
+                {key = "a", cb = tree_cb "create"},
+                {key = "d", cb = tree_cb "remove"},
+                {key = "r", cb = tree_cb "rename"},
+                {key = "<C-r>", cb = tree_cb "full_rename"},
+                {key = "x", cb = tree_cb "cut"},
+                {key = "c", cb = tree_cb "copy"},
+                {key = "p", cb = tree_cb "paste"},
+                {key = "y", cb = tree_cb "copy_name"},
+                {key = "Y", cb = tree_cb "copy_path"},
+                {key = "gy", cb = tree_cb "copy_absolute_path"},
+                {key = "[c", cb = tree_cb "prev_git_item"},
+                {key = "}c", cb = tree_cb "next_git_item"},
+                {key = "-", cb = tree_cb "dir_up"},
+                {key = "q", cb = tree_cb "close"},
+                {key = "g?", cb = tree_cb "toggle_help"}
+            }
+        }
     },
-    tab_open = 0,
-    allow_resize = 1,
-    add_trailing = 0, -- append a trailing slash to folder names
-    disable_netrw = 1,
-    hijack_netrw = 0,
-    update_cwd = 0,
-    bindings = {
-        {key = {"<CR>", "o", "<2-LeftMouse>"}, cb = tree_cb "edit"},
-        {key = {"<2-RightMouse>", "<C-]>"}, cb = tree_cb "cd"},
-        {key = "<C-v>", cb = tree_cb "vsplit"},
-        {key = "<C-x>", cb = tree_cb "split"},
-        {key = "<C-t>", cb = tree_cb "tabnew"},
-        {key = "<", cb = tree_cb "prev_sibling"},
-        {key = ">", cb = tree_cb "next_sibling"},
-        {key = "P", cb = tree_cb "parent_node"},
-        {key = "<BS>", cb = tree_cb "close_node"},
-        {key = "<S-CR>", cb = tree_cb "close_node"},
-        {key = "<Tab>", cb = tree_cb "preview"},
-        {key = "K", cb = tree_cb "first_sibling"},
-        {key = "J", cb = tree_cb "last_sibling"},
-        {key = "I", cb = tree_cb "toggle_ignored"},
-        {key = "H", cb = tree_cb "toggle_dotfiles"},
-        {key = "R", cb = tree_cb "refresh"}, {key = "a", cb = tree_cb "create"},
-        {key = "d", cb = tree_cb "remove"}, {key = "r", cb = tree_cb "rename"},
-        {key = "<C-r>", cb = tree_cb "full_rename"},
-        {key = "x", cb = tree_cb "cut"}, {key = "c", cb = tree_cb "copy"},
-        {key = "p", cb = tree_cb "paste"},
-        {key = "y", cb = tree_cb "copy_name"},
-        {key = "Y", cb = tree_cb "copy_path"},
-        {key = "gy", cb = tree_cb "copy_absolute_path"},
-        {key = "[c", cb = tree_cb "prev_git_item"},
-        {key = "}c", cb = tree_cb "next_git_item"},
-        {key = "-", cb = tree_cb "dir_up"}, {key = "q", cb = tree_cb "close"},
-        {key = "g?", cb = tree_cb "toggle_help"}
-    }
+    update_focused_file = {enabled = true},
+    auto_resize = true,
+    update_to_buf_dir = {enable = true}
 }
 
 local cmp = require 'cmp'
